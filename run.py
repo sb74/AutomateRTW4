6695e5dffb06c7d533f2571a2fbc37736782c293
Sean Brannan <snbr74@gmail.com>
2020-11-21 00:39:08 +0000

    JSON creator and uploader.

---
+++ run.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+import os
+import requests
+
+
+def create_json(content, filename):
+    new_json = {}
+
+    try:
+        new_json["name"] = content[0]
+        new_json["weight"] = content[1]
+        new_json["description"] = content[2]
+        new_json["image_name"] = filename
+    except KeyError:
+        print("File not in correct format.")
+
+    return new_json
+
+
+if __name__ == "__main__":
+    # External IP.
+    external_ip = "128.0.0.1"
+
+    # Linux home directory.
+    home = os.path.expanduser("~")
+    # Text file directory.
+    txt_dir = home + r"supplier-data/descriptions"
+
+    # Iterate through directory, create, and upload JSON.
+    for file in os.listdir(txt_dir):
+        file_path = os.path.join(txt_dir, file)
+        with open(file_path, "r") as description_file:
+            # Read in and strip feedback into a list and append to new list.
+            txt_content = description_file.readlines()
+            txt_content = [line.strip() for line in txt_content]
+            # Create dict using feedback content.
+            description_json = create_json(txt_content, file)
+
+            # Send feedback.
+            web_address = "http://" + external_ip + "/fruits/"
+            response = requests.post(web_address, json=description_json)
